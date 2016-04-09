ffbffd4992194d5485cc089a30c2447eacae1b57
Nick Pesce <npesce@terpmail.umd.edu>
2016-04-09 11:27:22 -0400

    POST requests now correctly update the game state

--- server/server.js
+++ server/server.js
@@ -18,13 +18,19 @@ app.post("/*/update_emoji", function(req, res) {
     console.log("update emoji from url: %s", path);
     var game = path.split("/")[1];
     console.log("game ID: %s", game);
-    console.log(req.body.emoji);
     gameStates[game]["emoji"] = req.body.emoji;
     console.log(JSON.stringify(gameStates));
+    res.send("Emoji updated\n");
 });
 
 app.post("/*/update_text", function(req, res) {
-    
+    var path = url.parse(req.url).pathname;
+    console.log("update text from url: %s", path);
+    var game = path.split("/")[1];
+    console.log("game ID: %s", game);
+    gameStates[game]["text"] = req.body.text;
+    console.log(JSON.stringify(gameStates));
+    res.send("Emoji updated\n");
 });
 
 var server = app.listen(8081, function() {
