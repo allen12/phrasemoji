771f9219a196005dd6ca4cca035395d798d537df
Cameron Moy <camoy@umd.edu>
2016-04-09 17:53:12 -0400

    Access and update ajax

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -22,18 +22,3 @@ $("#btn-back").click(function() {
 	$("textarea.emoji-input").val($("textarea.emoji-input").val().slice(0, -1));
 	$("div.emoji-input").children().last().remove();
 });
-
-$("#sub_btn").click(function() {
-	$(this).text("waiting on other players...");
-
-	var x = setInterval(function() { 
-		$.get("/get_results.json", function(data) {
-			if (data.finished) {
-				clearInterval(x);
-				$("#init_phrase").text('"' + data.phrase + '"');
-				$("#emoj_phrase").html(EmojiPicker.prototype.unicodeToImage(data.emoji));
-				$("#guess_phrase").text(data.text);
-			}
-		});
-	}, 2000);
-});
