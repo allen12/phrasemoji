8fd466349594a900ec7e02916416d4e9e570c872
Cameron Moy <camoy@umd.edu>
2016-04-09 13:09:24 -0400

    Frontend initial commit

---
+++ frontend/js/script.js
@@ -0,0 +1,22 @@
+$("#create_room").click(function() {
+	var $this = $(this);
+	$this.text("waiting...");
+
+	/* TODO: change GET to POST */
+	$.get("/request_room.json", function(data) {
+		if (data.id) {
+			$this.text("ready!");
+			$("#room_id").val("http://transloj.me/" + data.id + ".html");
+			$("#room_group").css({ display: "table" });
+		}
+	});
+});
+
+$("#go").click(function() {
+	window.location.href = $("#room_id").val();
+});
+
+$("#btn-back").click(function() {
+$("div.emoji-input").children().last().remove();
+
+});
