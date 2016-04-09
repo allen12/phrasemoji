3ab45baad9d646d35daff9426785845431724636
Nick Pesce <npesce@terpmail.umd.edu>
2016-04-09 10:54:25 -0400

    Added game state saving and entry points

--- server/server.js
+++ server/server.js
@@ -1,8 +1,30 @@
+var url = require("url");
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
+var gameStates = {};
+app.use(bodyParser.json());
+
 app.get("/", function(req, res) {
-    res.send("test");
+    game = "abcd";
+    res.send("Your unique url is: ");
+    gameStates[game] = {};
+    console.log(JSON.stringify(gameStates));
+});
+
+app.post("/*/update_emoji", function(req, res) {
+    var path = url.parse(req.url).pathname;
+    console.log("update emoji from url: %s", path);
+    var game = path.split("/")[1];
+    console.log("game ID: %s", game);
+    console.log(req.body.emoji);
+    gameStates[game]["emoji"] = req.body.emoji;
+    console.log(JSON.stringify(gameStates));
+});
+
+app.post("/*/update_text", function(req, res) {
+    
 });
 
 var server = app.listen(8081, function() {
