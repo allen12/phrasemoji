32e129f3b062e4034b8ab1e35c3017fa53ee8d20
Cameron Moy <camoy@umd.edu>
2016-04-09 21:47:21 -0400

    Client-side multiplayer logic

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,23 +1,3 @@
-$("#create_room").click(function() {
-	var $this = $(this);
-	if (!$this.attr("disabled")) {
-		$this.text("waiting...");
-	
-		$.getJSON("/request_room.json", function(data) {
-			if (data.id) {
-				$this.text("ready!");
-				$this.attr("disabled", true);
-				$("#room_id").val("http://phrasemoji.com/" + data.id);
-				$("#room_group").css({ display: "table" });
-			}
-		});
-	}
-});
-
-$("#go").click(function() {
-	window.location.href = $("#room_id").val();
-});
-
 $("#btn-back").click(function() {
 	$("textarea.emoji-input").val($("textarea.emoji-input").val().slice(0, -1));
 	$("div.emoji-input").children().last().remove();
